0f5cdce35668159bc981fb684ebb47556190c16d
LoveEmperor <1747098604@qq.com>
2018-09-05 10:48:13 +0800

    project init

---
+++ move.bat
@@ -0,0 +1,8 @@
+pushd "%~dp0"
+pushd "%cd%\"
+
+rem copy "源文件" "目标文件" /y
+
+rem 产品库对应关系
+copy "resources\shareimg.jpg" "ios" /y
+pause
---
+++ start.bat
@@ -0,0 +1,8 @@
+@echo on
+echo hello word
+:: ECHO newdoc>>doc
+:: md e:\mp4  
+set NowTime=%time%
+::读取起始时间，时间格式为：13:01:05.95
+echo 程序开始时间：%NowTime%
+pause
